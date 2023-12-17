bca593847dbbcc05700d69bccbb5a4e8b3cf655f
oychao <chao@ouyang.io>
2023-12-17 14:48:24 +0800

    feat: update vite-plugin-ts

--- templates/vite-plugin-ts/src/main.ts
+++ templates/vite-plugin-ts/src/main.ts
@@ -1,8 +1,15 @@
 import { PluginOption } from 'vite';
 
+export const VirtualPrefix = '\0';
+
 export function plugin(): PluginOption {
   const MyPlugin: PluginOption = {
     name: 'vite-plugin-ts-demo',
+    resolveId(source) {
+      return source;
+    },
+    // load(id, options) {},
+    // async transform(code, id, options) {},
   };
 
   return MyPlugin;
